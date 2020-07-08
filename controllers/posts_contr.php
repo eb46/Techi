9bfa4d855c46c74eab936578be3869a9a7a505c8
eb46 <ericjbondoc@gmail.com>
2020-07-08 10:09:27 -0700

    changes

--- controllers/posts_contr.php
+++ controllers/posts_contr.php
@@ -4,21 +4,21 @@
   header('Content-Type: application/json');
 
   if ($_REQUEST['action'] === 'index'){
-      echo json_encode(Posts::all());
+      echo json_encode(Reply::all());
   } else if ($_REQUEST['action'] === 'post'){
       $request_body = file_get_contents('php://input');
       $body_object = json_decode($request_body);
-      $new_post = new Post(null, $body_object->id, $body_object->reply);
-      $all_posts = Posts::create($new_post);
+      $new_post = new Post(null, $body_object->name, $body_object->reply);
+      $all_posts = Reply::create($new_post);
       echo json_encode($all_posts);
   } else if ($_REQUEST['action'] === 'update'){
       $request_body = file_get_contents('php://input');
       $body_object = json_decode($request_body);
       $updated_post = new Post($_REQUEST['id'], $body_object->name, $body_object->reply);
-      $all_posts = Posts::update($updated_post);
+      $all_posts = Reply::update($updated_post);
       echo json_encode($all_posts);
   } else if ($REQUEST['action'] === 'delete'){
-      $all_posts = Posts::delete($_REQUEST['id']);
+      $all_posts = Reply::delete($_REQUEST['id']);
       echo json_encode($all_posts);
   }
 
