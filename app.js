f9b07b839a3b53ccf91b724e4db1ed989449a9cc
Elektra Babian <babianelektra@gmail.com>
2020-07-08 16:27:37 -0400

    added edit and delte functions

--- app.js
+++ app.js
@@ -1,4 +1,48 @@
 class App extends React.Component {
+  state = {
+    replies: [],
+  }
+
+  deletePost = (event) => {
+    event.preventDefault();
+    axios.delete('/reply/' + event.target.value).then(
+      (response) => {
+        this.setState(
+          {
+            replies: response.data
+          }
+        )
+      }
+    )
+  }
+
+  editName = (event) => {
+    this.setState({
+      updateName: event.target.value
+    })
+  }
+
+  editReply = (event) => {
+    this.setState({
+      updateReply: event.target.value
+    })
+  }
+
+  updateReply = (event) => {
+    event.preventDefault();
+    const id = event.target.getAttribute('id');
+    axios.put(
+      '/reply/' + id,
+      {
+        name: this.state.updateName,
+        reply: this.state.updateReply,
+      }
+    ).then((response) => {
+      this.setState({
+        replies: response.data
+      })
+    })
+  }
 
   render = () => {
     return (<div>
