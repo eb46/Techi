2d200db2d1d17f68eaedbd3c333e63550d280d96
eb46 <ericjbondoc@gmail.com>
2020-07-09 07:48:20 -0700

    added topic backend data

--- models/posts_model.php
+++ models/posts_model.php
@@ -24,11 +24,13 @@ $dbconn = pg_connect("host=localhost dbname=techi");
 class Post {
   public $id;
   public $name;
+  public $topic;
   public $reply;
 
-  public function __construct($id, $name, $reply) {
+  public function __construct($id, $name, $topic, $reply) {
     $this->id = $id;
     $this->name = $name;
+    $this->topic = $topic;
     $this->reply = $reply;
   }
 }
@@ -44,6 +46,7 @@ class Reply {
       $new_reply = new Post(
         intval($row_object->id),
         $row_object->name,
+        $row_object->topic,
         $row_object->reply
       );
       $replies[] = $new_reply;
@@ -53,16 +56,16 @@ class Reply {
   }
 // create function
   static function create($reply){
-    $query = "INSERT INTO reply (name, reply) VALUES ($1, $2)";
-    $query_params = array($reply->name, $reply->reply);
+    $query = "INSERT INTO reply (name, topic, reply) VALUES ($1, $2, $3)";
+    $query_params = array($reply->name, $reply->topic, $reply->reply);
     pg_query_params($query, $query_params);
 
     return self::all();
   }
 // update function
   static function update($updated_reply){
-    $query = "UPDATE reply SET name= $1, reply = $2 WHERE id = $3";
-    $query_params = array($updated_reply->name, $updated_reply->reply, $updated_reply->id);
+    $query = "UPDATE reply SET name= $1, topic = $2, reply = $3 WHERE id = $4";
+    $query_params = array($updated_reply->name, $updated_reply->topic, $updated_reply->reply, $updated_reply->id);
     $result = pg_query_params($query, $query_params);
 
     return self::all();
